9371859c948452c37083a321ab4c6bef98e786fe
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-09-05 02:24:39 +0200

    feat: Initial commit

---
+++ src/main/java/pl/uncleglass/feeder/FeederApplication.java
@@ -0,0 +1,13 @@
+package pl.uncleglass.feeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeederApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/uncleglass/feeder/FeederApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uncleglass.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
