ab26f791d3feba3eefa8fc66764d225e3858e837
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-12-17 01:24:24 +0100

    feat: Deleted Test Controller
    It will not be needed anymore

--- src/main/java/pl/uncleglass/feeder/security/config/SecurityConfig.java
+++ src/main/java/pl/uncleglass/feeder/security/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/test", "/webjars/**").permitAll()
+                .antMatchers("/webjars/**").permitAll()
                 .anyRequest().authenticated();
 
         http.csrf().disable();
