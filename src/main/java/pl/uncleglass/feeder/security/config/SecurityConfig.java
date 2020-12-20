e29e67ca9ade44a2db8bc8a7503f7640fefdc2a5
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-12-20 08:44:42 +0100

    feat: Spring Security was disabled temporary

--- src/main/java/pl/uncleglass/feeder/security/config/SecurityConfig.java
+++ src/main/java/pl/uncleglass/feeder/security/config/SecurityConfig.java
@@ -1,38 +1,38 @@
-package pl.uncleglass.feeder.security.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import pl.uncleglass.feeder.security.service.AuthenticationService;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthenticationService authenticationService;
-
-    public SecurityConfig(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationService);
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/webjars/**").permitAll()
-                .anyRequest().authenticated();
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-
-        http.formLogin()
-                .permitAll();
-
-        http.formLogin()
-                .defaultSuccessUrl("/home", true);
-    }
-}
+//package pl.uncleglass.feeder.security.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import pl.uncleglass.feeder.security.service.AuthenticationService;
+//
+////@Configuration
+////@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final AuthenticationService authenticationService;
+//
+//    public SecurityConfig(AuthenticationService authenticationService) {
+//        this.authenticationService = authenticationService;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) {
+//        auth.authenticationProvider(authenticationService);
+//    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/webjars/**").permitAll()
+//                .anyRequest().authenticated();
+//
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+//
+//        http.formLogin()
+//                .permitAll();
+//
+//        http.formLogin()
+//                .defaultSuccessUrl("/home", true);
+//    }
+//}
