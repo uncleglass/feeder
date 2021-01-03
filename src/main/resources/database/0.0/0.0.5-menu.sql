9f70ffb346dc2731e62b666032c97c106382a97d
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-03 23:49:22 +0100

    feat: Added tables for Day Menu

---
+++ src/main/resources/database/0.0/0.0.5-menu.sql
@@ -0,0 +1,18 @@
+--liquibase formatted sql
+--changeset uncleglass:1
+
+CREATE TABLE day_menus
+(
+    id   uuid NOT NULL DEFAULT uuid_generate_v4(),
+    date date,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE day_menu_meals
+(
+    day_menu_id  uuid NOT NULL,
+    meal_id      uuid NOT NULL,
+    meal_type_id integer,
+    FOREIGN KEY (day_menu_id) REFERENCES day_menus (id),
+    FOREIGN KEY (meal_id) REFERENCES meals (id),
+);
