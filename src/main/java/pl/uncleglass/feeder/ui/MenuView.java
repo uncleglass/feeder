e1b2adc4444861c7b718c8be7710a9809dce5468
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-22 16:23:21 +0100

    feat: Updated UI Main View

--- src/main/java/pl/uncleglass/feeder/ui/MenuView.java
+++ src/main/java/pl/uncleglass/feeder/ui/MenuView.java
@@ -1,22 +1,26 @@
 package pl.uncleglass.feeder.ui;
 
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import pl.uncleglass.feeder.backend.adapters.vaadin.DayMenuAdapter;
 import pl.uncleglass.feeder.backend.adapters.vaadin.DayMenuDto;
-import pl.uncleglass.feeder.backend.adapters.vaadin.MenuAdapter;
+
+import java.time.LocalDate;
 
 @Route(value="", layout = MainLayout.class)
 //
 @PageTitle("Menu | Karmink")
 @CssImport("shared-styles.css")
 public class MenuView extends VerticalLayout {
-    private final MenuAdapter menuAdapter;
+    private final DayMenuAdapter dayMenuAdapter;
+    private final Label label = new Label();
 
 
-    public MenuView(MenuAdapter menuAdapter) {
-        this.menuAdapter = menuAdapter;
+    public MenuView(DayMenuAdapter dayMenuAdapter) {
+        this.dayMenuAdapter = dayMenuAdapter;
         setSizeFull();
 
         showDay();
@@ -24,7 +28,9 @@ public class MenuView extends VerticalLayout {
     }
 
     private void showDay() {
-
+        DayMenuDto dayMenu = dayMenuAdapter.getDayMenu(LocalDate.now());
+        label.setText(dayMenu.getDate().toString());
+        add(label);
     }
 
 }
