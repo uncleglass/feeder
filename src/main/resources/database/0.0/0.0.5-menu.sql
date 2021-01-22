92de7cb43e78db3f3a4cc2c412551a6c9a06f4c2
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-22 16:24:15 +0100

    feat: Updated DB schema

--- src/main/resources/database/0.0/0.0.5-menu.sql
+++ src/main/resources/database/0.0/0.0.5-menu.sql
@@ -16,3 +16,21 @@ CREATE TABLE day_menu_meals
     FOREIGN KEY (day_menu_id) REFERENCES day_menus (id),
     FOREIGN KEY (meal_id) REFERENCES meals (id)
 );
+
+--changeset uncleglass:2
+
+ALTER TABLE day_menu_meals
+    RENAME TO day_menus_meals;
+
+--changeset uncleglass:3
+
+DROP TABLE day_menus_meals;
+
+CREATE TABLE day_menus_meals
+(
+    day_menu_id  uuid NOT NULL,
+    meals_id      uuid NOT NULL,
+    meals_key integer,
+    FOREIGN KEY (day_menu_id) REFERENCES day_menus (id),
+    FOREIGN KEY (meals_id) REFERENCES meals (id)
+);
