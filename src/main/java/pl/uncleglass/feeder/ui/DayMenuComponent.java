b9582fb84437350ccd3cf3c421372e4e28230125
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-23 23:05:58 +0100

    feat: Added displaying name of meals

--- src/main/java/pl/uncleglass/feeder/ui/DayMenuComponent.java
+++ src/main/java/pl/uncleglass/feeder/ui/DayMenuComponent.java
@@ -1,17 +1,75 @@
 package pl.uncleglass.feeder.ui;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import pl.uncleglass.feeder.backend.adapters.vaadin.DayMenuDto;
+import pl.uncleglass.feeder.backend.adapters.vaadin.MealDto;
+
+import java.util.Map;
 
 public class DayMenuComponent extends Composite<Div> {
     private Label date;
+    private Map<String, MealDto> meals;
 
     public DayMenuComponent(DayMenuDto dayMenuDto) {
         date = new Label();
         date.setText(dayMenuDto.getDate().toString());
 
-        getContent().add(date);
+        meals = dayMenuDto.getMeals();
+
+        getContent().add(date, configureMealsDisplaying());
+    }
+
+    private Component configureMealsDisplaying() {
+        VerticalLayout layout = new VerticalLayout();
+
+        HorizontalLayout breakfast = new HorizontalLayout();
+        breakfast.add(new Label("Ś"));
+        TextField breakfastName = new TextField();
+        breakfastName.setReadOnly(true);
+        MealDto beakfastValue = meals.get("Śniadanie");
+        breakfastName.setValue(beakfastValue == null ? "" : beakfastValue.getName());
+        breakfast.add(breakfastName);
+
+        HorizontalLayout branch = new HorizontalLayout();
+        branch.add(new Label("D"));
+        TextField branchName = new TextField();
+        branchName.setReadOnly(true);
+        MealDto branchValue = meals.get("Drugie śniadanie");
+        branchName.setValue(branchValue == null ? "" : branchValue.getName());
+        branch.add(branchName);
+
+        HorizontalLayout lunch = new HorizontalLayout();
+        lunch.add(new Label("O"));
+        TextField lunchName = new TextField();
+        lunchName.setReadOnly(true);
+        MealDto lunchValue = meals.get("Obiad");
+        lunchName.setValue(lunchValue == null ? "" : lunchValue.getName());
+        lunch.add(lunchName);
+
+        HorizontalLayout snack = new HorizontalLayout();
+        snack.add(new Label("P"));
+        TextField snackName = new TextField();
+        snackName.setReadOnly(true);
+        MealDto snackValue = meals.get("Przekąska");
+        snackName.setValue(snackValue == null ? "" : snackValue.getName());
+        snack.add(snackName);
+
+        HorizontalLayout supper = new HorizontalLayout();
+        supper.add(new Label("K"));
+        TextField supperName = new TextField();
+        supperName.setReadOnly(true);
+        MealDto supperValue = meals.get("Kolacja");
+        supperName.setValue(supperValue == null ? "" : supperValue.getName());
+        supper.add(supperName);
+
+        layout.add(breakfast, branch, lunch, snack, supper);
+
+        return layout;
     }
 }
