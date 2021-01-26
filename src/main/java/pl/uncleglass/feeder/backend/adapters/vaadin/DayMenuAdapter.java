018248b816b99672fe5ce082885e3b8340cc7fc0
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-26 17:50:39 +0100

    feat: Added addMealToDayMenu() method to vaadin DayMenuAdapter

--- src/main/java/pl/uncleglass/feeder/backend/adapters/vaadin/DayMenuAdapter.java
+++ src/main/java/pl/uncleglass/feeder/backend/adapters/vaadin/DayMenuAdapter.java
@@ -2,16 +2,33 @@ package pl.uncleglass.feeder.backend.adapters.vaadin;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.uncleglass.feeder.backend.app.meal.domain.MealType;
+import pl.uncleglass.feeder.backend.app.menu.port.in.AddMealToDayMenuUseCase;
 import pl.uncleglass.feeder.backend.app.menu.port.in.GetDayMenuUseCase;
 
 import java.time.LocalDate;
+import java.util.UUID;
+
+import static pl.uncleglass.feeder.backend.app.menu.port.in.AddMealToDayMenuUseCase.*;
 
 @Service
 @RequiredArgsConstructor
 public class DayMenuAdapter {
     private final GetDayMenuUseCase getDayMenuUseCase;
+    private final AddMealToDayMenuUseCase addMealToDayMenuUseCase;
 
     public DayMenuDto getDayMenu(LocalDate date) {
         return DayMenuMapper.mapToDto(getDayMenuUseCase.getDayMenu(date));
+    }
+
+    public void addMealToDayMenu(UUID dayMenuId, String mealTypeStr, UUID menuId) {
+        MealType mealType = MealMapper.convertMealTypeStr(mealTypeStr);
+        addMealToDayMenuUseCase.addMealToDayMenu(
+                new AddMealToDayMenuCommand(
+                        dayMenuId,
+                        mealType,
+                        menuId
+                )
+        );
     }
 }
