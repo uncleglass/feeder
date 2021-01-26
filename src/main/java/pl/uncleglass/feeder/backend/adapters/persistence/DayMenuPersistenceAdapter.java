e477289e80f7643e6370c82f4b90eb9ad8b4a75f
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-26 17:19:49 +0100

    feat: Overloaded leadDayMenu() method to find Day Menu by Id

--- src/main/java/pl/uncleglass/feeder/backend/adapters/persistence/DayMenuPersistenceAdapter.java
+++ src/main/java/pl/uncleglass/feeder/backend/adapters/persistence/DayMenuPersistenceAdapter.java
@@ -6,7 +6,9 @@ import pl.uncleglass.feeder.backend.app.menu.domain.DayMenu;
 import pl.uncleglass.feeder.backend.app.menu.port.out.CreateDayMenuPort;
 import pl.uncleglass.feeder.backend.app.menu.port.out.LoadDayMenuPort;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +26,12 @@ class DayMenuPersistenceAdapter implements CreateDayMenuPort, LoadDayMenuPort {
     public DayMenu loadDayMenu(LocalDate date) {
         DayMenuJpaEntity byDate = dayMenuRepository.findDayMenuJpaEntitiesByDate(date);
         return dayMenuMapper.mapToDomainEntity(byDate);
+    }
+
+    @Override
+    public DayMenu loadDayMenu(UUID dayMenuId) {
+        DayMenuJpaEntity dayMenuJpaEntity = dayMenuRepository.findById(dayMenuId)
+                .orElseThrow(EntityNotFoundException::new);
+        return dayMenuMapper.mapToDomainEntity(dayMenuJpaEntity);
     }
 }
