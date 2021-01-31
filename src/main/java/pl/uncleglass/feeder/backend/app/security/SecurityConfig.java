6bf8fd9cc9d636cae335f5e93d03ad752c68b736
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2021-01-31 18:27:00 +0100

    feat: Temporary commented security features

--- src/main/java/pl/uncleglass/feeder/backend/app/security/SecurityConfig.java
+++ src/main/java/pl/uncleglass/feeder/backend/app/security/SecurityConfig.java
@@ -1,44 +1,44 @@
-package pl.uncleglass.feeder.backend.app.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthenticationService authenticationService;
-
-    public SecurityConfig(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationService);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**", "/h2-console/**").permitAll()
-
-                .and()
-                .csrf().disable()
-                .headers().frameOptions().disable();
-
-//        http.csrf().disable()
-//                .requestCache().requestCache(new CustomRequestCache())
-//                .and().authorizeRequests()
-//                .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
-//                .anyRequest().authenticated();
-
-        http.formLogin()
-                .permitAll();
-
-        http.formLogin()
-                .defaultSuccessUrl("/", true);
-    }
-}
+//package pl.uncleglass.feeder.backend.app.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final AuthenticationService authenticationService;
+//
+//    public SecurityConfig(AuthenticationService authenticationService) {
+//        this.authenticationService = authenticationService;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) {
+//        auth.authenticationProvider(authenticationService);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/**", "/h2-console/**").permitAll()
+//
+//                .and()
+//                .csrf().disable()
+//                .headers().frameOptions().disable();
+//
+////        http.csrf().disable()
+////                .requestCache().requestCache(new CustomRequestCache())
+////                .and().authorizeRequests()
+////                .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
+////                .anyRequest().authenticated();
+//
+//        http.formLogin()
+//                .permitAll();
+//
+//        http.formLogin()
+//                .defaultSuccessUrl("/", true);
+//    }
+//}
--- src/main/java/pl/uncleglass/feeder/backend/app/user/User.java
+++ src/main/java/pl/uncleglass/feeder/backend/app/user/User.java
@@ -4,20 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.UUID;
 
-@Entity
-@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue
+
     private UUID id;
     private String username;
     private String salt;
